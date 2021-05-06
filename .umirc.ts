be420c49636cb0493ff931f6abe6e37547f9d418
juguohong <juguohong@Sanzo.local>
2021-05-06 15:25:41 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  layout: {},
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/detail', component: '@/pages/detail' },
+  ],
+  fastRefresh: {},
+});
---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'umi';
+
+const Detail = () => {
+  return (
+    <div>
+      <h3>detail</h3>
+      <Link to="/">首页</Link>
+    </div>
+  );
+};
+
+export default Detail;
